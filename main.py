d5981b6014f08a9f647a8bc68ac5b137926d6b12
Kiki Adi Pamungkas <92570748+kurumi64@users.noreply.github.com>
2022-05-10 15:49:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,3 @@ telur.set_nama("Telur Asin (Banget)")
 telur.set_harga(13000)
 telur.show_produk()
 
-
-minyak1 = Minyak()
-minyak1.set_nama("Minyak Eceran")
-minyak1.set_harga(56500)
-minyak1.show_produk()
-
-print("=" * 30)
-
-minyak2 = Minyak()
-minyak2.set_nama("Minyak Bimoli")
-minyak2.set_harga(1000000)
-minyak2.show_produk()

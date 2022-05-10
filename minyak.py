4450268d21caa567ec343a3a27a2c0eb6bc58611
Kiki Adi Pamungkas <92570748+kurumi64@users.noreply.github.com>
2022-05-10 15:20:36 +0700

    tambah minyak

---
+++ minyak.py
@@ -0,0 +1,12 @@
+from produk import Produk 
+
+class Minyak(Produk):
+    
+    def set_name(self, nama: str):
+        self.get_nama()
+
+    def get_nama(self):
+        print("Bimoli")
+    
+    def show_produk():
+        print(" ")

23b090cda6fc9497981fd120e540593e111a41b7
Kiki Adi Pamungkas <92570748+kurumi64@users.noreply.github.com>
2022-05-10 15:37:09 +0700

    hapus " ") "

--- minyak.py
+++ minyak.py
@@ -2,4 +2,4 @@ from produk import Produk
 
 class Minyak(Produk):
     def show_produk(self):
-        print(f"Nama Produk: {self.get_nama()}\nHarga: {self.get_harga()}")")
+        print(f"Nama Produk: {self.get_nama()}\nHarga: {self.get_harga()}")

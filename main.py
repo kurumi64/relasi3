d8a78305d705f7eecc748b8202443582aa164454
Kiki Adi Pamungkas <92570748+kurumi64@users.noreply.github.com>
2022-05-10 15:56:40 +0700

    penambahan minyak1 dan minyak2

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ telur.set_nama("Telur Asin (Banget)")
 telur.set_harga(13000)
 telur.show_produk()
 
+minyak1 = Minyak()
+minyak1.set_nama("minyak eceran")
+minyak1.set_harga(30000)
+minyak1.show_produk()
+
+print("=" * 30)
+
+minyak2 = Minyak()
+minyak2.set_nama("minyak bimoli")
+minyak2.set_harga(1000000)
+minyak2.show_produk()

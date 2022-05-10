cd5ac0d474ccb5644df730b4f969c3d51907af9e
Kiki Adi Pamungkas <92570748+kurumi64@users.noreply.github.com>
2022-05-10 15:49:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telur import Telur
+from minyak import Minyak
 
 telur = Telur()
 telur.set_nama("Telur Asin")
@@ -10,3 +11,16 @@ print("=" * 30)
 telur.set_nama("Telur Asin (Banget)")
 telur.set_harga(13000)
 telur.show_produk()
+
+
+minyak1 = Minyak()
+minyak1.set_nama("Minyak Eceran")
+minyak1.set_harga(56500)
+minyak1.show_produk()
+
+print("=" * 30)
+
+minyak2 = Minyak()
+minyak2.set_nama("Minyak Bimoli")
+minyak2.set_harga(1000000)
+minyak2.show_produk()
